791eaddc08677c1bf949e01e76557d957b61505f
Augusto Boschiazzo Marinelli <augus2608@gmail.com>
2024-04-13 16:09:47 -0300

    Cambios punto 6 TP 2

--- practicas/src/tp2/Transformacion.java
+++ practicas/src/tp2/Transformacion.java
@@ -4,13 +4,25 @@ public class Transformacion {
 	
 	public static BinaryTree<Integer> suma(BinaryTree<Integer> a) {
 		int suma = 0;
+		BinaryTree<Integer> nuevo = null;
 		if (!a.isEmpty()) {
 			
 		}
+		return nuevo;
 	}
 	
-	public static int recorrido(BinaryTree<Integer> a, int suma) {
-		
+	public static int recorrido(BinaryTree<Integer> a) {
+		int n = a.getData();
+		int izq = 0;
+		int der = 0;
+		if (a.hasLeftChild()) {
+			izq = recorrido(a.getLeftChild());
+		}
+		if (a.hasRightChild()) {
+			der = recorrido(a.getRightChild());
+		}
+		a.setData(izq + der);
+		return n + izq + der;
 	}
 	
 
